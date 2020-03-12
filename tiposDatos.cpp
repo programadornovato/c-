5ec201e91d617c8755289a0d5fa69faaa0e57181
Programador Novato <50611379+programadornovato@users.noreply.github.com>
2020-03-12 10:06:24 +0100

    04.- C++ desde cero 2019🦸‍♂️ [Tipos de datos en c++]
    Video:
    https://youtu.be/oXmFtnTabZc

---
+++ tiposDatos.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    int entero=10;
+    float flotante=9.99;
+    double doble=9.988989898;
+    char caracter='c';
+    std::cout<<caracter;
+    std::cin.get();
+    return 0;
+}
