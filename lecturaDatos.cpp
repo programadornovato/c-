8c6a0d42e3e35bcebfadd5d08803e062aec93da1
Programador Novato <50611379+programadornovato@users.noreply.github.com>
2020-03-16 23:15:48 +0100

    05.- C++ desde cero 2019🦸‍♂️ [Lectura de datos en C++]
    Video:
    https://youtu.be/g9ybtpNC71U

---
+++ lecturaDatos.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    float numero;
+    std::cout<<"Humano!!! ingresa un numero entero: ";
+    std::cin>>numero;
+    std::cout<<"Humano este es el numero que ingresaste:";
+    std::cout<<numero;
+    std::cout<<"\n";
+    //std::cin.get();
+    system("pause");
+    return 0;
+}
