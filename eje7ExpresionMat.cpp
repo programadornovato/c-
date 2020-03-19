e029f1c919278a3bc228d2a89224c0552efb3bc3
Programador Novato <50611379+programadornovato@users.noreply.github.com>
2020-03-19 16:12:01 -0600

    14.- C++ desde cero 2019🦸‍♂️ [Validar un numero con la condicional if]
    Video:
    https://youtu.be/GdNjvSZkU6A

--- eje7ExpresionMat.cpp
+++ eje7ExpresionMat.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <math.h>
 int main(){
-    float x,y,res;
+    float x,y,res,yCuadradaMenosUno;
     std::cout<<"Humano digita lo que se te pida \n";
     std::cout<<"Valor de x: ";std::cin>>x;
+    ingresaYdeNuevo:
     std::cout<<"Valor de y: ";std::cin>>y;
-    res=(sqrt(x)) / (pow(x,2)-1);
+    yCuadradaMenosUno=(pow(y,2)-1);
+    if(yCuadradaMenosUno==0){
+        std::cout<<"Humano estupido el valor de y no es valido ingresa de nuevo el valor de y\n";
+        goto ingresaYdeNuevo;
+    }
+    res=(sqrt(x)) / yCuadradaMenosUno;
     std::cout<<"Aqui esta tu chingadera de resultado: "<<res<<"\n";
     system("pause");
     return 0;
