3662ab3d36d815095f85766a616e6331be8d18d4
Programador Novato <50611379+programadornovato@users.noreply.github.com>
2020-04-25 02:18:26 -0500

    104.-C++ desde cero 2019🦸‍♂️ [Quitar un nodo de la pila]
    Video:
    https://youtu.be/y_QibvKGBWU

--- pilas.cpp
+++ pilas.cpp
@@ -5,10 +5,23 @@ struct Nodo
     Nodo *siguiente;
 };
 void insertaNodo(Nodo *&pila,int n);
+void quitarNodo(Nodo *&pila,int &n);
 int main(){
     Nodo *pila=NULL;
+    int numero;
     insertaNodo(pila,5);
     insertaNodo(pila,6);
+    insertaNodo(pila,7);
+    while (pila!=NULL)
+    {
+        quitarNodo(pila,numero);
+        if(pila!=NULL){
+            std::cout<<numero<<" , ";
+        }else{
+            std::cout<<numero<<" . \n";
+        }
+    }
+    
     system("pause");
     return 0;
 }
@@ -18,4 +31,10 @@ void insertaNodo(Nodo *&pila,int n){
     nuevoNodo->siguiente=pila;
     pila=nuevoNodo;
     std::cout<<"El dato "<<n<<" fue insertado exitosamente\n";
+}
+void quitarNodo(Nodo *&pila,int &n){
+    Nodo *aux=pila;
+    n=aux->dato;
+    pila=aux->siguiente;
+    delete aux;
 }
