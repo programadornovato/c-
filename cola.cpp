4a580ad43d8a37022a471ac58802c844ebcad749
Programador Novato <50611379+programadornovato@users.noreply.github.com>
2020-04-28 20:32:29 -0500

    110.-C++ desde cero 2019🦸‍♂️ [Mostrar elementos de la cola en C++]
    Video:
    https://youtu.be/j4HRrxeUQ3g

--- cola.cpp
+++ cola.cpp
@@ -5,11 +5,14 @@ struct Nodo
     Nodo *siguiente;
 };
 void insertarNodo(Nodo *&front,Nodo *&back,int dato);
+void mostrarNodos(Nodo *&front);
 int main(){
     Nodo *front=NULL;
     Nodo *back=NULL;
     insertarNodo(front,back,5);
     insertarNodo(front,back,6);
+    insertarNodo(front,back,7);
+    mostrarNodos(front);
     system("pause");
     return 0;
 }
@@ -26,4 +29,12 @@ void insertarNodo(Nodo *&front,Nodo *&back,int dato){
         back=nuevoNodo;
     }
     std::cout<<"Se inserto un nodo con el dato "<<dato<<"\n";
+}
+void mostrarNodos(Nodo *&front){
+    while (front!=NULL)
+    {
+        std::cout<<"Dato: "<<front->dato<<"\n";
+        front=front->siguiente;
+    }
+    
 }
