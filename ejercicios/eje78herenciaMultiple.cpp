7bdd2137a565f3de4e3a1c31fbea2b7090f9c02f
Programador Novato <50611379+programadornovato@users.noreply.github.com>
2020-05-12 19:50:06 -0500

    151.- C++ desde cero 2019🦸‍♂️ [Ejercicio 78.- Herencia 2da parte]
    Video:
    https://youtu.be/OL9hN_xlcFc

--- ejercicios/eje78herenciaMultiple.cpp
+++ ejercicios/eje78herenciaMultiple.cpp
@@ -41,8 +41,35 @@ Estudiante::Estudiante(char _nombre[],int _edad,float _calificacion):Persona(_no
 void Estudiante::mostrarEstudiante(){
     std::cout<<"Calificacion:"<<calificacion<<"\n";
 }
-
+class Universitario:public Estudiante{
+    private:
+        char *carrera;
+    public:
+        Universitario(char _nombre[],int _edad,float _calificacion,char _carrera[]);
+        void mostrarUniversitario();
+};
+Universitario::Universitario(char _nombre[],int _edad,float _calificacion,char _carrera[]):Estudiante(_nombre,_edad,_calificacion){
+    carrera=_carrera;
+}
+void Universitario::mostrarUniversitario(){
+    std::cout<<"Carrera: "<<carrera<<"\n";
+}
 int main(){
+    Persona p1("Euegenio",30);
+    p1.mostrarPersona();
+    std::cout<<"________________\n";
+    Empleado e1("Juan",20,15000);
+    e1.mostrarPersona();
+    e1.mostrarEmpleado();
+    std::cout<<"________________\n";
+    Estudiante es1("Maria",18,10);
+    es1.mostrarPersona();
+    es1.mostrarEstudiante();
+    std::cout<<"________________\n";
+    Universitario u1("Juana",25,9,"Informatica");
+    u1.mostrarPersona();
+    u1.mostrarEstudiante();
+    u1.mostrarUniversitario();
     system("pause");
     return 0;
 }
